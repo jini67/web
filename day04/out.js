587e85d980f766e9004d247fed662a451eec3be8
jini67 <124111020+jini67@users.noreply.github.com>
2023-02-02 17:34:04 +0900

    day04 프로젝트

---
+++ day04/out.js
@@ -0,0 +1,16 @@
+        //함수는 여러 절차가 있는 처리를 이름을 부여하여 기능을 정의!
+        function 빨래하다() {
+            console.log("1. 먼지를 턴다.")
+            console.log("2. 물에 집어 넣는다.")
+            console.log("3. 비누를 바른다.")
+            console.log("4. 옷끼리 비빈다.")
+            console.log("5. 물에 여러번 헹군다.")
+            console.log("6. 물기를 짠다")
+            console.log("7. 빨래를 넌다")
+        }
+
+        function 청소하다() {
+            console.log("1. 물건을 치운다.")
+            console.log("2. 청소기를 돌린다.")
+            console.log("3. 물걸레를 한다.")
+        }
---
+++ day04/out2.js
@@ -0,0 +1,15 @@
+        //함수는 여러 절차가 있는 처리를 이름을 부여하여 기능을 정의!
+        function 여행하다() {
+            console.log("1. 여행지 선정.")
+            console.log("2. 계획짜기.")
+            console.log("3. 항공권 예매.")
+            console.log("4. 숙소예약.")
+            console.log("5. 출발.")
+            console.log("6. 도착")
+        }
+
+        function 쉬다() {
+            console.log("1. 눕는다.")
+            console.log("2. 눈감는다.")
+            console.log("3. 잔다.")
+        }
