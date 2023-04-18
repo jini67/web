722b9d33e6a4108ece7f9cee146555e9d4f6862d
Koo yoonjin <124111020+jini67@users.noreply.github.com>
2023-04-18 17:53:07 +0900

    day57 프로젝트
    컬렉션

---
+++ K번째큰수.java
@@ -0,0 +1,28 @@
+package 컬렉션2;
+
+import java.util.Arrays;
+
+public class K번째큰수 {
+
+	public static void main(String[] args) {
+		int[] num = {3, 4, 5, 1, 2};
+		int k = 2; //2번째 큰 수
+		// 일단 정렬하기!
+		// 정렬한 데이터 중에서 2번째 위치를 찾을 것
+		Arrays.sort(num); // 원본을 정렬해서 다시 저장
+		for (int x : num) {
+			System.out.print(x + " ");
+		}
+		System.out.println();
+		String num2 = Arrays.toString(num); // Arrays.toString(num); ==> num을 보기좋게 배열로 만들어주는것
+		System.out.println(num2);
+		System.out.println(Arrays.toString(num)); //num2와 같은것
+		
+		//System.out.println("가장 큰 수 " + num[4]);
+		System.out.println("가장 큰 수 " + num[num.length - 1]);
+		System.out.println("두번째 큰 수 " + num[num.length - 2]);
+		System.out.println("세번째 큰 수 " + num[num.length - 3]);
+		System.out.println(k + "번째 큰 수 " + num[num.length - k]);
+	}
+
+}
